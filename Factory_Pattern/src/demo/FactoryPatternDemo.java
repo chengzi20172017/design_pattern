f59ff1fb74aebdc4c377c39a642c6a1066e842f6
chengzi <909115655@qq.com>
2018-07-21 16:49:56 +0800

    添加东西

--- Factory_Pattern/src/demo/FactoryPatternDemo.java
+++ Factory_Pattern/src/demo/FactoryPatternDemo.java
@@ -2,7 +2,7 @@ package demo;
 
 import api.Shape;
 import factory.ShapeFactory;
-
+//入口
 public class FactoryPatternDemo {
 	public static void main(String[] args) {
 	      ShapeFactory shapeFactory = new ShapeFactory();
